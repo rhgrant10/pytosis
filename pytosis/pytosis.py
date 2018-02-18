22f2ad1c323d11499ac9e8a81fbfe5a697b30e25
Robert Graham <rpgraham84@gmail.com>
2018-02-17 20:52:50 -0500

    Simple models

--- pytosis/pytosis.py
+++ pytosis/pytosis.py
@@ -1,3 +1,32 @@
 # -*- coding: utf-8 -*-
 
-"""Main module."""
+
+class Gene:
+    pass
+
+
+class Creature:
+    def __init__(self, genes):
+        self.genes = genes
+
+
+class Node:
+    def __init__(self, radius, weight, friction):
+        self.radius = radius
+        self.weight = weight
+        self.friction = friction
+
+
+class Muscle:
+    def __init__(self, node_a, node_b, strength, length, group):
+        self.node_a = node_a
+        self.node_b = node_b
+        self.strength = strength
+        self.length = length
+        self.group = group
+
+
+
+
+
+
