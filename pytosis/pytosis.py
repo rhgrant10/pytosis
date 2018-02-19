f9fcbb4aa854c9e1e5e4060da5a32219703c7a14
Robert Grant <rhgrant10@gmail.com>
2018-02-19 06:45:21 -0800

    Starting off

--- pytosis/pytosis.py
+++ pytosis/pytosis.py
@@ -1,35 +1,44 @@
 # -*- coding: utf-8 -*-
+import time
 
-
-class Gene:
-    pass
-
-
-class Creature:
-    def __init__(self, genes):
-        self.genes = genes
+import pymunk
 
 
 class Node:
-    def __init__(self, radius, weight, friction):
-        self.radius = radius
-        self.weight = weight
-        self.friction = friction
+    def __init__(self, position, mass, radius, friction):
+        moment = pymunk.moment_for_circle(mass, 0, radius)
+        self.body = pymunk.Body(mass, moment)
+        self.body.position = position
+        self.shape = pymunk.Circle(self.body, radius)
+        self.shape.friction = friction
+
+    def build(self, space):
+        space.add(self.body, self.shape)
 
 
 class Muscle:
-    def __init__(self, node_a, node_b, strength, length, group):
-        self.node_a = node_a
-        self.node_b = node_b
-        self.strength = strength
-        self.length = length
-        self.group = group
+    def __init__(self, a, b, period, stiffness, damping=0):
+        rest_length = a.position.get_distance(b.position)
+        self.constraint = pymunk.DampedSpring(a, b, (0, 0), (0, 0),
+                                              rest_length, stiffness, damping)
+
+    def build(self, space):
+        space.add(self.constraint)
+
+    @property
+    def ideal_length(self):
+        return 0 if self.is_contracting() else self.rest_length
+
+    def is_contracting(self):
+        return (time.time() % self.period) / self.period < .5
 
 
+class Creature:
+    def __init__(self, nodes, muscles):
+        self.nodes = nodes
+        self.muscles = muscles
 
 
-
-
-git staticmethodgit staticmethoasd
-
-d
+def main():
+    space = pymunk.Space()
+    space.gravity = (0, -900)
