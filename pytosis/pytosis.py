25d01f9c0ae5ec3238aaa5847d93d40ef9db0143
Robert Graham <rpgraham84@gmail.com>
2018-02-18 21:31:04 -0500

    Basic genes, nodes, muscles, and creatures

--- pytosis/pytosis.py
+++ pytosis/pytosis.py
@@ -1,35 +1,116 @@
 # -*- coding: utf-8 -*-
+import random
+
+from itertools import zip_longest
 
 
 class Gene:
-    pass
+    """
+    Sequence is expected to be a sequence of ones and zeros as a string.
+    Makes it easier for coding for now.
+    Genetic sequences go in even rotation for now. Node-Muscle-Node
+
+    """
+
+    def __init__(self, sequence):
+        self.sequence = sequence
+
+    def __iter__(self):
+        """
+        Iterating a Gene yields nibbles - 4 bit codons
+
+        FIXME: Should this be zero-filled at the end? Cuz it is.
+
+        """
+        seq = [self.sequence[x::4] for x in range(4)]
+        for codon in zip_longest(*seq, fillvalue="0"):
+            yield ''.join(codon)
+
+    @classmethod
+    def from_random(cls):
+        # Init the Gene with a random number 31 to 32 bits long
+        return cls(bin(random.randint(2 ** 31, 2 ** 32))[2:])
+
+    @classmethod
+    def from_creature(cls, creature):
+        return [feature.to_codon for feature in creature.features]
+
+    def to_creature(self):
+        return Creature(self)
 
 
-class Creature:
-    def __init__(self, genes):
-        self.genes = genes
+class CodonsToValuesMixin:
+    @staticmethod
+    def codons_to_values(codons, num_vals):
+        size = len(codons) // num_vals
+        for i in range(num_vals):
+            yield int(codons[i * size:size * (i + 1)], 2) + 1
 
 
-class Node:
+class Node(CodonsToValuesMixin):
+    """
+    Nodes have radius, weight and friction.
+    """
     def __init__(self, radius, weight, friction):
         self.radius = radius
         self.weight = weight
         self.friction = friction
 
+    def to_codon(self):
+        pass
 
-class Muscle:
-    def __init__(self, node_a, node_b, strength, length, group):
-        self.node_a = node_a
-        self.node_b = node_b
+    @classmethod
+    def from_codons(cls, codons):
+        print(f"Node <{codons}>")
+        radius, weight, friction = cls.codons_to_values(codons, 3)
+        print(f"radius {radius}, weight: {weight}, friction: {friction}")
+        return cls(radius, weight, friction)
+
+
+class Muscle(CodonsToValuesMixin):
+    """
+
+    """
+
+    def __init__(self, strength, length, group):
         self.strength = strength
         self.length = length
         self.group = group
 
+    @classmethod
+    def from_codons(cls, codons):
+        print(f"Muscle <{codons}>")
+        strength, length, group = cls.codons_to_values(codons, 3)
+        print(f"strength {strength}, length: {length}, group: {group}")
+        return cls(strength, length, group)
 
 
+class Creature:
+    """
+    These creatures are assumed (for now) to follow the structure
+    Node-Muscle-Node-Muscle...
+
+    """
+    features = [Node, Muscle]
+
+    def __init__(self, gene):
+        self.genes = gene
+        self.parts = []
+        len_features = len(self.features)
+        codons = []
+        for i, codon in enumerate(self.genes):
+            cls = self.features[i % len_features]
+            codons.append(codon)
+
+            # If the codon ends with a one, read the next one
+            if codon[-1] != '1':
+                self.parts.append(cls.from_codons(''.join(codons)))
+                codons = []
+
+    @classmethod
+    def from_random(cls):
+        return cls(Gene.from_random())
 
 
-
-git staticmethodgit staticmethoasd
-
-d
+if __name__ == "__main__":
+    Creature.from_random()
